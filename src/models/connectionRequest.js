00f662177fbed7b24f60cde0541551a5de849964
Hasir Ali <hasira804@gmail.com>
2024-11-06 14:18:50 +0530

    request send api

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -11,12 +11,20 @@ const connectionRequestSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ['pending', 'accepted', 'rejected', 'interested'], 
+        enum: ['pending', 'accepted', 'rejected', 'interested','ignored'], 
         required: true,
     },
 }, {
     timestamps: true,
 });
+connectionRequestSchema.pre("save",function(next){
+    const connectionRequest = this;
+    // check if fromUSerId and toUserId are same
+    if(connectionRequest.fromUserId.toString() === connectionRequest.toUserId.toString()){
+        throw new Error("You cannot send request to yourself");
+    }
+    next();
+})
 
 const ConnectionRequestModel = mongoose.model('ConnectionRequest', connectionRequestSchema);
 module.exports = ConnectionRequestModel;
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const requestRouter = express.Router(); 
+const requestRouter = express.Router();
 const userAuth = require('../middlewares/auth');
 const ConnectionRequestModel = require('../models/ConnectionRequest'); // Import your model
+const UserModel = require('../models/user');
 
 // Sending Connection Request
 requestRouter.post('/request/send/:status/:touserId',
@@ -12,17 +13,65 @@ requestRouter.post('/request/send/:status/:touserId',
             const toUserId = req.params.touserId; // ID of the user receiving the request
             const status = req.params.status; // Status of the request
 
+            const allowedStatus = ["interested", "ignored"];
+            if (!allowedStatus.includes(status)) {
+                return res.status(400).json({ error: "Invalid status" });
+            }
+
+
+            // check if a user is sending a request to himself
+            // if (fromUserId._id.toString() === toUserId.toString()) {
+            //     return res.status(400).json({ error: "You cannot send a request to yourself" });
+            // }
+
+
+
+            
+            // check if ther toUserId is valid(mean real or fake)
+            const toUser = await UserModel.findById(toUserId);
+            if(!toUser){
+                return res.status(400).json({ error: "User Not Found" });
+            }
+                
+            
+
+            // check if there is already a pending request between the two users
+            // also check if request is already sent by the other user to me(loggedIn user)
+            const existingConnectionRequest = await ConnectionRequestModel.findOne({
+                $or: [
+                    // check if there is already a pending request between the two users
+                    {
+                        toUserId,fromUserId 
+                        // if toUseId and fromUserId already exist then we wont send req
+
+
+                        // or
+                        // toUserId: toUserId,
+                        // fromUserId: fromUserId
+                    },
+                    // also check if request is already sent by the other user to me(loggedIn user)
+                    {
+                        toUserId: fromUserId,
+                        fromUserId: toUserId
+                    }
+                ]
+            })
+            if(existingConnectionRequest){
+                return res.status(400).send({message: "Request already sent"});
+            }
+ 
 
             const connectionRequest = new ConnectionRequestModel({
                 fromUserId,
                 toUserId,
                 status,
             });
-            const data = await connectionRequest.save(); 
+            const data = await connectionRequest.save();
 
             console.log(`${req.user.firstName} request sent`);
+        
             res.json({
-                message: "Connection Request sent successfully",
+                message: req.user.firstName + " is " + status + " in " + toUser.firstName,
                 data,
             });
         }
