74e6e0b4682c37ead233dd0a3c0f54b5e1d14400
Hasir Ali <hasira804@gmail.com>
2024-11-11 00:20:44 +0530

    hideuserFromFeed Function created

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const userRouter = express.Router();
 const userAuth = require('../middlewares/auth')
-const connectionRequestModel = require('../models/connectionRequest')
+const connectionRequestModel = require('../models/connectionRequest');
+const { connection } = require('mongoose');
 
 
 // api to get all the pending requests of a loggedInUser user[working]
@@ -18,7 +19,7 @@ userRouter.get('/user/requests/received', userAuth, async (req, res) => {
         const connectionRequests = await connectionRequestModel.find({
             toUserId: loggedInUser._id,
             status: "interested",
-        }).populate("fromUserId", ["firstName", "lastName", "photoUrl" ,"bio" ,"skills"]);
+        }).populate("fromUserId", ["firstName", "lastName", "photoUrl", "bio", "skills"]);
         res.json({
             message: "data Fetched Successfully",
             data: connectionRequests
@@ -29,17 +30,18 @@ userRouter.get('/user/requests/received', userAuth, async (req, res) => {
     }
 })
 
+// api to get all the connection [working]
 userRouter.get('/user/connections', userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
         const connections = await connectionRequestModel.find({
-            $or:[
+            $or: [
                 { toUserId: loggedInUser._id, status: "accepted" },
                 { fromUserId: loggedInUser._id, status: "accepted" },
             ]
         }).populate("fromUserId", "firstName lastName photoUrl about skills")
 
-        const data = connections.map((e)=>{
+        const data = connections.map((e) => {
             return e.fromUserId;
         })
         res.json({
@@ -50,6 +52,52 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
     }
     catch (err) {
         res.status(404).send("Error fetching connections" + err.message);
+    }
+})
+
+// api to build feed page
+userRouter.get('/user/feed', userAuth, async (req, res) => {
+
+
+    try {
+        // not seeing my own Profile(Card)
+        // not seeing the post of the user i already send intreseted or ignored
+        // not seeing the post of the user who i have already connected
+        const loggedInUser = req.user
+        // find all connection requsest sent or received 
+        const connectionRequests = await connectionRequestModel.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId")  // these are the users who are already connected or sent request to or received request from
+        // .populate("fromUserId", "firstName")
+        // .populate("toUserId", "firstName ")
+        // so i will hide these user from feed
+        const hideUserFromFeed = new Set();
+        connectionRequests.forEach((e)=>{
+            hideUserFromFeed.add(e.fromUserId.toString())
+            hideUserFromFeed.add(e.toUserId.toString())
+        })
+        // console.log(hideUserFromFeed);
+
+        // find all the users who are not in the hideUserFromFeed
+        const user = await UserModel.find({
+            _id: {$nin: Array.from(hideUserFromFeed)}  //id is NotIn the array hideUserFromFeed
+        })
+        res.json({
+            message: "data Fetched Successfully",
+            data: connectionRequests
+        })
+    }
+
+
+
+
+
+
+    catch (err) {
+        res.status(404).send("Error fetching feed" + err.message);
     }
 })
 
